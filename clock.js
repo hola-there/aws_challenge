3dfef18fb875a758a550082d21982287b535ee91
hola-there <22028053+hola-there@users.noreply.github.com>
2021-08-03 14:28:02 -0400

    Finished adding flipclock code

--- clock.js
+++ clock.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   var clock = $('.lambda-clock').FlipClock({
   	clockFace: 'TwelveHourClock'
   });
-  / need to provide the value from the lambda here
-  clock.setFaceValue(value);
+  clock.stop();
+  // need to provide the value from the lambda here
+  // clock.setFaceValue(value);
 });
